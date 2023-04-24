ecbe224687e4a3cae8588317cb320aa7e449b89d
kavikannavkl <kavikannavkl@gmail.com>
2023-04-24 21:59:34 +0530

    first commit

---
+++ ifelse.py
@@ -0,0 +1,9 @@
+n = int(input("enter a number : "))
+if(n%2!=0):
+ print("Weird")
+elif(n%2==0 and n in range(2,6)):
+   print("Not Weird")
+elif(n%2==0 and n in range(6,21)):
+ print("Weird")
+else:
+ print("Not Weird")
