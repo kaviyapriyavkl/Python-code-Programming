193a6090086ed0f50349f6a7db4db90231517ebf
kavikannavkl <kavikannavkl@gmail.com>
2023-04-26 15:12:21 +0530

    first commit

---
+++ stringconcat.py
@@ -0,0 +1,6 @@
+def print_full_name(first, last):
+      print("Hello",first,last+"! You just delved into python." )
+
+first_name = input("Enter your firstname : ")
+last_name = input("Enter your lastname : ")
+print_full_name(first_name, last_name)
